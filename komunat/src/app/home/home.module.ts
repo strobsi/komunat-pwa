4cbc89bb2e1a1c45feb824c7df3c8291dc93508c
Simon Strobel <simon@codebrew.de>
2019-04-26 06:41:37 +0200

    asd

--- komunat/src/app/home/home.module.ts
+++ komunat/src/app/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
+import { IonicStorageModule } from '@ionic/storage';
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    IonicStorageModule.forRoot(),
     IonicModule,
     RouterModule.forChild([
       {
